d3d3af1158b3fb8bbaf256a71b4bd2ff69f8056a
AleksandrButakov <anbngm@gmail.com>
2023-05-03 14:04:25 +0500

    Changed code

---
+++ src/test/java/tests/smartdeal/TestBase.java
@@ -0,0 +1,68 @@
+package tests.smartdeal;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.*;
+import static helpers.Attach.*;
+import static com.codeborne.selenide.Selenide.sessionId;
+
+class TestBase {
+
+    @BeforeAll
+    static void beforeAll() throws MalformedURLException {
+        baseUrl = "https://smartdeal.pro";
+        browser = "chrome";
+        browserVersion = "112.0";
+        browserSize = "1920x1080";
+        remote = "http://62.113.108.218:4444/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            /* How to add test badge */
+            put("name", "My tests https://smartdeal.pro");
+
+            /* How to set session timeout */
+            put("sessionTimeout", "15m");
+
+            /* How to set timezone */
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            /* How to enableVNC */
+            put("enableVNC", true);
+
+            /* How to enable video recording */
+            put("enableVideo", true);
+
+        }});
+        browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open(baseUrl);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        String sessionId = sessionId().toString(); //getSessionId();
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        browserConsoleLogs();
+        closeWebDriver();
+        attachVideo(sessionId);
+    }
+
+}
---
+++ src/test/java/tests/smartdeal/TestMainPage.java
@@ -0,0 +1,127 @@
+package tests.smartdeal;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class TestMainPage extends TestBase {
+    @Test
+    @Tag("myTest")
+    @DisplayName("Проверка кнопки 'Банкам'")
+    void checkingTheButtonForBanks() {
+        step("Checking the button for banks", () -> {
+            $$(".tn-atom").findBy(text("Банкам")).click();
+            $$(".tn-atom").findBy(text("Мы предлагаем"))
+                    .shouldHave(text("Мы предлагаем"));
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка кнопки 'Застройщикам'")
+    void checkingTheButtonForDevelopers() {
+        step("Checking the button for developers", () -> {
+            $$(".tn-atom").findBy(text("Застройщикам")).click();
+            $$(".tn-atom").findBy(text("Мы предлагаем"))
+                    .shouldHave(text("Мы предлагаем"));
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка кнопки 'Получить консультацию'")
+    void checkingTheGetAdviceButton() {
+        step("Checking the button for developers", () -> {
+            $$(".tn-atom").findBy(text("Получить консультацию")).click();
+            $$(".tn-atom").findBy(text("Получить консультацию"))
+                    .shouldHave(text("Получить консультацию"));
+        });
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("Проверка ссылки 'Блог'")
+    void  checkingTheBlogLink() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("Проверка ссылки 'Команда'")
+    void checkingTheLinkCommand() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("Проверка ссылки 'Контакты'")
+    void checkingTheContactsLink() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("Проверка ссылки 'Справочный центр'")
+    void checkingTheHelpCenterLink() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("Проверка ссылки 'Тарифы'")
+    void checkingTheLinkRates() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("Проверка ссылки 'Правовая информация'")
+    void linkVerificationLegalInformation() {
+
+    }
+
+    @Disabled
+    @Test
+    void successfulRegistrationTest() {
+        step("Open registrations form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+        });
+
+        step("Fill form", () -> {
+            $("#firstName").setValue("Alex");
+            $("#lastName").setValue("Egorov");
+            $("#userEmail").setValue("alex@egorov.com");
+            $("#genterWrapper").$(byText("Other")).click(); // best
+            $("#userNumber").setValue("1234567890");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("July");
+            $(".react-datepicker__year-select").selectOption("2008");
+            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#uploadPicture").uploadFromClasspath("img/1.png");
+            $("#currentAddress").setValue("Some address 1");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
+        });
+
+        step("Check form results", () -> {
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
+                    text("alex@egorov.com"), text("1234567890"));
+        });
+
+    }
+}
