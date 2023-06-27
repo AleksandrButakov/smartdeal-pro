1cb5c02bf294c3e3d359b28e16c924a958ad1331
AleksandrButakov <anbngm@gmail.com>
2023-06-27 09:24:21 +0500

    change code owner

--- src/test/java/TypeTest.java
+++ src/test/java/TypeTest.java
@@ -9,10 +9,15 @@ public class TypeTest {
     @Test
     public void testInteger() {
         System.setProperty("integer", "10");
-
         TypeConfig config = ConfigFactory.create(TypeConfig.class, System.getProperties());
-
         assertThat(config.getInteger()).isEqualTo(10);
+    }
+
+    @Test
+    public void testDouble() {
+        System.setProperty("double", "10.10");
+        TypeConfig config = ConfigFactory.create(TypeConfig.class, System.getProperties());
+        assertThat(config.getDouble()).isEqualTo(10.10);
     }
 
 }
