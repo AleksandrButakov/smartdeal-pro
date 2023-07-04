407d014c4f23588b73141e03411ec8ea27df3fab
AleksandrButakov <anbngm@gmail.com>
2023-07-04 23:50:19 +0500

    add WebDriverConfig.java interface

--- src/test/java/config/RemoteConfig.java
+++ src/test/java/config/RemoteConfig.java
@@ -2,8 +2,13 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.Sources({
+        "classpath:testing.properties"
+})
+
 public interface RemoteConfig extends Config {
 
-
+    @Key("webdriver.baseUrl")
+    String getWebDriverBaseUrl();
 
 }
--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 import java.net.URL;
 
 @Config.Sources({
-        "classpath:mobile.properties"
+        "classpath:testing.properties"
 })
 
 public interface WebDriverConfig extends Config {
